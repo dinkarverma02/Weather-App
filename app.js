f7d0a899fbe43a74ec9b9d16f73b8b472455f599
dinkarverma02 <dinkarverma2002@gmail.com>
2022-09-09 17:41:23 -0400

    adding files

---
+++ app.js
@@ -0,0 +1,112 @@
+let weather = {
+    apiKey: "78ce5ad4c3a48b1364a5f14c01df9fe8",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://www.openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".searchBar").value);
+    },
+  };
+  
+  let geocode = {
+    reverseGeocode: function (latitude, longitude) {
+      var api_key = "b59654d505974420b219fc647d75b072";
+      var api_url = "https://api.opencagedata.com/geocode/v1/json";
+  
+      var request_url =
+        api_url +
+        "?" +
+        "key=" +
+        api_key +
+        "&q=" +
+        encodeURIComponent(latitude + "," + longitude) +
+        "&pretty=1" +
+        "&no_annotations=1";
+  
+      // see full list of required and optional parameters:
+      // https://opencagedata.com/api#forward
+  
+      var request = new XMLHttpRequest();
+      request.open("GET", request_url, true);
+  
+      request.onload = function () {
+        // see full list of possible response codes:
+        // https://opencagedata.com/api#codes
+  
+        if (request.status === 200) {
+          // Success!
+          var data = JSON.parse(request.responseText);
+          // console.log(data.results[0]);
+          weather.fetchWeather(data.results[0].components.city);
+        } else if (request.status <= 500) {
+          // We reached our target server, but it returned an error
+          console.log("unable to geocode! Response code: " + request.status);
+          var data = JSON.parse(request.responseText);
+          console.log("error msg: " + data.status.message);
+        } else {
+          console.log("server error");
+        }
+      };
+  
+      request.onerror = function () {
+        // There was a connection error of some sort
+        console.log("unable to connect to server");
+      };
+  
+      request.send(); // make the request
+    },
+  
+    // getLocation: function () {
+    //   function success(data) {
+    //     geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+    //   }
+  
+    //   if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(success, console.error);
+    //   }
+    //   else {
+    //     weather.fetchWeather("Tokyo");
+    //   }
+    // },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".searchBar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  // By default website shows Toronto's Weather
+  weather.fetchWeather("Toronto");
+  
+  // geocode.getLocation();
